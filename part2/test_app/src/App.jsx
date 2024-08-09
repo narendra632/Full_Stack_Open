3850b7cca3f839cb8c73731fc0fb0e3473b3459e
Narendra Singh Rathore <mrperfectth7@gmail.com>
2024-08-10 00:03:25 +0530

    Update: to change the importance of notes

--- part2/test_app/src/App.jsx
+++ part2/test_app/src/App.jsx
@@ -33,11 +33,25 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
+      important: Math.random() > 0.5,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+  }
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
   }
 
 
@@ -50,9 +64,15 @@ const App = () => {
         </button>
       </div>
       <ul>
+
         {notesToShow.map(note =>
-          <Note key={note.id} note={note}/>
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
          )}
+
       </ul>
 
       <form onSubmit={addNote}>
