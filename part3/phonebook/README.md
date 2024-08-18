6d73b5b2d36057e426193d173e52349c80fad3db
Narendra Singh Rathore <mrperfectth7@gmail.com>
2024-08-18 13:35:58 +0530

    Update: frontend added to backend deploy

---
+++ part3/phonebook/README.md
@@ -0,0 +1,1 @@
+https://phonebook-i69l.onrender.com
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -23,6 +23,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
 
