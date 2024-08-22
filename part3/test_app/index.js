941210d50c559432e853323f8d5500c27064102c
Narendra Singh Rathore <mrperfectth7@gmail.com>
2024-08-22 17:21:58 +0530

    Update: test_app delete and update

--- part3/test_app/index.js
+++ part3/test_app/index.js
@@ -4,9 +4,9 @@ const app = express()
 require('dotenv').config()
 const Note = require('./models/note')
 
-app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 app.get('/', (request, response) => {
@@ -21,27 +21,27 @@ app.get('/api/notes', (request, response) => {
 })
 
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if(note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
-// Function to generate a new id
-const gererateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String (maxId + 1)
-}
 // Add a new note
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -61,6 +61,39 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
+
+
+// Update a post
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 
 
